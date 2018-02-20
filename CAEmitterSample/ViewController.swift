8c724e28a12ace9eb0a9292da6f8070dcf0ac3ae
Manohar Kurapati <kvmanohar@gmail.com>
2018-02-20 21:31:26 +0000

    Initial Commit

--- CAEmitterSample/ViewController.swift
+++ CAEmitterSample/ViewController.swift
@@ -12,13 +12,55 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        createParticales()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Local function
+    func createParticales(){
+        
+        let particleEmitter = CAEmitterLayer()
+        
+        particleEmitter.emitterPosition = CGPoint(x: view.center.x, y: -96)
+        particleEmitter.emitterShape = kCAEmitterLayerLine
+        particleEmitter.emitterSize = CGSize(width: view.frame.size.width, height: 1)
+        
+        let red = makeEmitterCell(color: UIColor.red)
+        let green = makeEmitterCell(color: UIColor.green)
+        let blue = makeEmitterCell(color: UIColor.blue)
+        
+        particleEmitter.emitterCells = [red, green, blue]
+        
+        view.layer.addSublayer(particleEmitter)
+        
     }
+    
+    func makeEmitterCell(color: UIColor) -> CAEmitterCell{
+        let cell = CAEmitterCell()
+        
+        cell.birthRate = 3
+        cell.lifetime = 7.0
+        cell.lifetimeRange = 0
+        cell.color = color.cgColor
+        cell.velocity = 200
+        cell.velocityRange = 50
+        cell.emissionLongitude = CGFloat.pi
+        cell.emissionRange = CGFloat.pi / 4
+        cell.spin = 2
+        cell.spinRange = 3
+        cell.scaleRange = -0.5
+        cell.scaleSpeed = -0.05
+        
+//        cell.contents = UIImage(named: "snow")?.cgImage
+        cell.contents = UIImage(named: "Rectangle")?.cgImage
+        return cell
+        
+    }
+
 
 
 }
